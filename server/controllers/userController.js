e0b8183bf7a8bfacfd5d79816b5bd039d0aeee6e
neeraj678singh <neerajsinghoime678@gmail.com>
2025-09-11 17:35:36 +0530

    user controller for profile update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import { generateToken } from "../lib/utils";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary";
 
 // controller to sign up a user
 export const signup = async (req, res)=>{
@@ -46,5 +47,31 @@ export const login = async (req, res)=>{
     }catch(error){
         console.log(error.message);
         res.json({success: false, message: error.message})
+    }
+}
+
+// controller to check if user is authenticated or not
+
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user: req.user});
+}
+
+// controller to update user profile details
+export const updateProfile = async (req, res)=>{
+    try{
+        const {profilePic, bio, fullName} = req.body;
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true});
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new: true});
+            res.json({success: true, user: updatedUser});
+        }
+    }catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message});
     }
 }
