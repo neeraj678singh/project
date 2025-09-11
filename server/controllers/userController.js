89b01451dc2e4946fd885bdb2d9c4c6df56f8655
neeraj678singh <neerajsinghoime678@gmail.com>
2025-09-11 17:01:56 +0530

    utils and userController created ,hence signup successfully handled

---
+++ server/controllers/userController.js
@@ -0,0 +1,27 @@
+import User from "../models/User";
+
+export const signup = async (req, res)=>{
+    const {fullName, email, password, bio} = req.body;
+
+    try{
+        if(!fullName || !email || !password || !bio){
+            return res.json({success: false, message: "Missing Details"});
+        }
+        const user = await User.findOne({email});
+        if(user){
+            return res.json({success: false, message: "Account already exists"}); 
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword, bio
+        });
+        const token =  generateToken(newUser._id);
+        
+        res.json({success: true, userData: newUser, token, message: "Account created successfully"});
+    }catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
