1fe77968bc545903de031b0db22eb35616b637e5
neeraj678singh <neerajsinghoime678@gmail.com>
2025-09-12 12:16:21 +0530

    message sending controller created and socket.io integrated to server

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,7 @@
 import Message from "../models/Message";
 import User from "../models/User";
+import cloudinary from "../lib/cloudinary";
+import { io, userSocketMap } from "../server";
 
 
 
@@ -59,3 +61,34 @@ export const markMessageAsSeen = async (req, res)=>{
     }
 }
 
+// Send message to selected user
+export const sendMessage = async (req, res)=>{
+    try{
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        // Emit the new message to the receiver's socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
+        res.json({success: true, newMessage});
+    }catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+} 
--- server/server.js
+++ server/server.js
@@ -5,11 +5,39 @@ import http from "http";
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
+import {Server} from 'socket.io';
+
+
 
 // Create Express app and HTTP server
-
 const app = express();
 const server = http.createServer(app);
+
+// initailize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+});
+
+// Store online users
+export const userSocketMap = {};
+
+// Socket.io connection handler
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+
+})
 
 // Middleware Setup
 app.use(express.json({limit: "4mb"}));
