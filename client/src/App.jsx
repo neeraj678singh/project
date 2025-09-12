a01d7ae7e20613900789a91ac1c050a443d69bd5
neeraj678singh <neerajsinghoime678@gmail.com>
2025-09-12 14:21:04 +0530

    AuthContext created and inside AuthProvider checkAuth completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,13 @@
   import HomePage from './pages/HomePage';
   import ProfilePage from './pages/ProfilePage';
   import LoginPage from './pages/LoginPage';
+  import {Toaster} from 'react-hot-toast';
+
 
   const App = () => {
     return (
       <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+        <Toaster/>
         <Routes>
               <Route path='/' element={<HomePage />} />
               <Route path='/login' element={<LoginPage />} />
